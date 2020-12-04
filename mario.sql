39ff329f0a3e2e38b306dc96986a0f87aea10657
gtm232 <74996247+gtm232@users.noreply.github.com>
2020-12-03 18:56:05 -0800

    Update mario.sql

--- mario.sql
+++ mario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2020 at 05:22 PM
+-- Generation Time: Dec 04, 2020 at 06:53 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.3
 
@@ -571,6 +571,34 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `name`, `email`, `email_verified_at`, `password`, `hak_akses`, `gambar`, `penanggung_jawab`, `pt`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'ario', 'ario', 'ariodw44@gmail.com', NULL, '$2y$10$mwZGudegyHIKioP4utNUT.cLHpDrOKvDoobj/KMMS/TF9WKijKrFO', 'admin', '(52503) IMG_0451 copy.JPG', '', 'PT PGN', 'fO1ok2c98MmxLcAxW61laMWc7KSJJGbEFIILNdxwHBUdZQDRHxFwn8BM7X03', '2019-03-29 00:47:55', '2020-12-02 21:45:55'),
+(5, 'anang.susanto', 'Anang Susanto', 'Sony.Achmad@pgn.co.id', NULL, '$2y$10$M5T67lA8AqWRpQyJS/izpuqXuPbNIKvKqg9lbcy8JBxV6ZKQxOAj.', 'user', '(82785) pgnpgn.PNG', '', 'PT PGN', NULL, '2019-06-10 01:12:37', '2019-06-10 01:12:37'),
+(6, 'mahar.prasetyo', 'Mahar Prasetyo', 'Mahar.Prasetyo@pgn.co.id', NULL, '$2y$10$akWTsooZz.WdKCzSG6yvZ.PLiEw18zu0EFWmxMAk2FAL0rwn6wWEy', 'user', '(80279) mahar.jpg', '', 'PT PGN', NULL, '2019-06-10 01:14:37', '2019-06-10 01:14:37'),
+(7, 'sony.achmad', 'Sony Achmad', 'Sony.Achmad@pgn.co.id', NULL, '$2y$10$B1KAXjkEk2TaRexWkS.ENe766QvX9jm8FDFkSxWoBFM4r2vv3ETRa', 'user', '(56364) pgnpgn.PNG', 'WOJBB', 'PT PGN', NULL, '2019-07-25 20:27:54', '2019-07-25 20:27:54'),
+(8, 'akrom.wibowo', 'Akrom Akhmadi Wibowo', 'Akrom.Wibowo@pgn.co.id', NULL, '$2y$10$BsVENslpP4u0n2ud2gO4Y.2HVnGPERzy3sKILl6ogbmARPAlzW/1m', 'user', '(66095) akrom.jpg', '', 'PT PGN', NULL, '2019-07-29 02:24:19', '2019-07-29 02:24:19'),
+(9, 'haris.wicaksono', 'Haris Wicaksono', 'Haris.Wicaksono@pgn.co.id', NULL, '$2y$10$Cd9QvHJb44sHgasvcjPD4OdpV6kDtxep7i3poIYsWsnrjPEfhUX52', 'user', '(38782) pgnpgn.PNG', '', 'PT PGN', NULL, '2019-07-29 17:34:19', '2019-07-29 17:34:19'),
+(10, 'citra.pranandar', 'Citra Pranandar', 'Citra.Pranandar@pgn.co.id', NULL, '$2y$10$aoZLJbsVtxGaLKqOqdlo0ukTYkuVPWI.ABfikR9Br879Bw1hO1H9a', 'Eproc', '(40677) pgnpgn.PNG', '', 'PT PGN', NULL, '2019-07-30 19:12:09', '2019-07-30 19:12:09'),
+(11, 'farah.diba', 'Farah Diba', 'Secretary.GTM@pgn.co.id', NULL, '$2y$10$VPIlkq4zs.gmLfl8i.VqSORZtxFbYlfZMmrwXUbPpmO5D99LH6gs6', 'user', '(43042) pgnpgn.PNG', '', 'PT PGN', NULL, '2019-07-30 23:37:57', '2019-07-30 23:37:57'),
+(12, 'agung.kurniansyah', 'Agung Rahmat Kurniansyah', 'Agung.Kurniansyah@pgn.co.id', NULL, '$2y$10$qRwalZg7os51jQOAhfigQuM6JLMu39xjTdTnxHlyHdQx76xz8hqhG', 'Koordinator', '(96952) pgnpgn.PNG', '', 'PT PGN', NULL, '2019-07-31 00:34:22', '2019-07-31 00:34:22'),
+(13, 'emi.hermanto', 'Emi Heru Hermanto', 'emi.hermanto@pgn.co.id', NULL, '$2y$10$o3irWG5C33GxPRA20Ao1D.vNrLINfu7ZVtLumAJWtXhZmvuzRS.e.', 'user', '(73518) Jellyfish.jpg', '', 'PT PGN', NULL, '2019-10-01 19:39:00', '2019-10-01 19:39:00'),
+(15, 'santoso.pratomo', 'Santoso Priyo Pratomo', 'santoso.pratomo@pgn.co.id', NULL, '$2y$10$xIjjjwYes8nN7lb.XplRUu5L2/x/KEhKBT64f/IwuPOWWs4/qJn0C', 'user', '(11134) pgnpgn.PNG', 'WOSS', 'PT PGN', NULL, '2019-10-14 21:27:38', '2019-10-14 21:27:38'),
+(16, 'aditya.eka', 'Aditya Eka Wijaya', 'aditya.eka@pgn.co.id', NULL, '$2y$10$vPWChSblZiRvBPhS2QR84.ldSRJMexd4NhIqvU8Q/ZSFTgfy79ZVu', 'user', '(60583) Penguins.jpg', '', 'PT PGN', NULL, '2019-11-12 23:37:33', '2019-11-12 23:37:33'),
+(17, 'faris.muhtadi', 'Faris Muhtadi', 'faris.muhtadi@pgn.co.id', NULL, '$2y$10$iz3So/5OwQwLYdvVQwgG2.nhb0EsFYGibZYQ7FAkL./X8QL95./52', 'user', '(20280) Penguins.jpg', '', 'PT PGN', NULL, '2019-11-15 01:01:51', '2019-11-15 01:01:51'),
+(18, 'henki.rizulianta', 'Henki Rizulianta', 'henki.rizulianta@pgn.co.id', NULL, '$2y$10$NyqYWyPWtbVZtD7Gw/6xrO.vqciaRpAGGE3ZH3r4y4HPQpF7JqYPO', 'user', '(47624) ipc9.png', '', 'PT PGASOL', NULL, '2019-11-17 07:59:23', '2019-11-17 07:59:23'),
+(19, 'hari.aribowo', 'Hari Satria Aribowo', 'hari.aribowo@pgn.co.id', NULL, '$2y$10$8.uVIWh6x5oWKMl6zWAozuS5Ni.8Ptiedp0Xv0PBGBXcriJyJEi.O', 'user', '(42352) Hydrangeas.jpg', '', '', NULL, '2019-11-26 20:16:43', '2019-11-26 20:16:43'),
+(20, 'imron.rosyidi', 'Imron Rosyidi', 'imronadi25@gmail.com', NULL, '$2y$10$K9rYq94fmvmNwL8OabDwae/oDY.99K.wnQB2hGAyM3/046RCSobQK', 'user', '(31920) Screenshot_2019-12-13-11-24-57-125_com.whatsapp.jpg', '', '', NULL, '2019-12-12 21:26:23', '2019-12-12 21:26:23'),
+(21, 'made.artama', 'I Made Yudi Artama', 'Made.Artama@pgn.co.id', NULL, '$2y$10$LDuKFl9wULYLYqfZSfTAse1YVECtfueGZThbi41xGM8nvxeVLl6Vm', 'user', '(14253) pp.jpg', '', '', NULL, '2019-12-12 23:38:29', '2019-12-12 23:38:29'),
+(22, 'denita', 'denita', 'denita@pgn.co.id', NULL, '$2y$10$FvZQY2X/xIIIQPiUtIeKvO/Xz0OxExh5Kn.NUrbxPhk7Px9hoSKF.', 'user', '(79492) Hydrangeas.jpg', '', '', NULL, '2019-12-16 00:43:49', '2019-12-16 00:43:49'),
+(23, 'sony.sungkawa', 'Sony Sungkawa', 'gtmmario@gmail.com', NULL, '$2y$10$pO1mBsiUdGc7jJ0m344Ixu6LqS3C.UWRhBZMS1I8aOtMoFnmV8fO6', 'user', '(13822) unnamed.jpg', '', '', NULL, '2019-12-22 19:14:06', '2020-05-03 17:46:58'),
+(24, 'hari.satria', 'Hari Satria', 'hari.satria@pgn.co.id', NULL, '$2y$10$PBc.elqOJ2yRQliGrgTdouzHRMCEq2PUB9G9l8Kdouenh6lXkuOnO', 'user', '(45732) Tulips.jpg', '', '', NULL, '2020-01-29 21:11:39', '2020-01-29 21:11:39'),
+(25, 'mira', 'Mira Fatmawati', 'mira.fatmawati@pgn.co.id', NULL, '$2y$10$ksa/n7xTq3L3p/VvUgR2kuMLmjG5qFoeMyicpQ11s8LSPmffyQip6', 'user', '(21958) Tulips.jpg', '', '', NULL, '2020-07-13 00:06:00', '2020-07-28 14:28:14'),
+(27, 'dea', 'dea', 'dea@gmail.com', NULL, '$2y$10$Ij5Ws7xgjdzoksWI396DYu2prZItd2/L.mUs08.X9OVHpQazwoIFq', 'user', '(68890) (56364) pgnpgn.png', '', '', NULL, '2020-07-30 00:02:41', '2020-07-30 00:02:41');
+
 -- --------------------------------------------------------
 
 --
@@ -1039,7 +1067,7 @@ ALTER TABLE `surat_surat`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `user_review_catatan_revisisi`
